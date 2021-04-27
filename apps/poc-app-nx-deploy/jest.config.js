3a46802e98aef21d3bcd557bf6f21b980d291551
Martin Meyer <martin.meyer@inftec.ch>
2021-04-27 09:52:27 +0200

    initial commit

---
+++ apps/poc-app-nx-deploy/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  displayName: 'poc-app-nx-deploy',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
+  coverageDirectory: '../../coverage/apps/poc-app-nx-deploy',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/poc-app-nx-deploy', '<rootDir>/apps/api'],
+};
