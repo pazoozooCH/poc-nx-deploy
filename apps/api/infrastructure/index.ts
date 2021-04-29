97f6500373ad1e8b88d9c2f086f2eb90ec16b567
Martin Meyer <martin.meyer@inftec.ch>
2021-04-29 11:35:29 +0200

    Reverted back to plugin created project

--- apps/api/infrastructure/index.ts
+++ apps/api/infrastructure/index.ts
@@ -1,20 +1,29 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as resources from "@pulumi/azure-native/resources";
-import * as storage from "@pulumi/azure-native/storage";
+import * as azure from '@pulumi/azure';
+import * as pulumi from '@pulumi/pulumi';
 
-// Create an Azure Resource Group
-const resourceGroup = new resources.ResourceGroup("resourceGroup");
+const stackConfig = new pulumi.Config();
+const config = {
+  // ===== DONT'T TOUCH THIS -> CONFIG REQUIRED BY nx-deploy-it ======
+  projectName: stackConfig.get('projectName')
+  // ===== END ======
+};
+const projectName = config.projectName;
 
-// Create an Azure resource (Storage Account)
-const storageAccount = new storage.StorageAccount("sa", {
-    resourceGroupName: resourceGroup.name,
-    sku: {
-        name: storage.SkuName.Standard_LRS,
-    },
-    kind: storage.Kind.StorageV2,
-});
+const resourceGroup = new azure.core.ResourceGroup(`${projectName}-rg`);
 
-// Export the primary key of the Storage Account
-const storageAccountKeys = pulumi.all([resourceGroup.name, storageAccount.name]).apply(([resourceGroupName, accountName]) =>
-    storage.listStorageAccountKeys({ resourceGroupName, accountName }));
-export const primaryStorageKey = storageAccountKeys.keys[0].value;
+const nodeApp = new azure.appservice.ArchiveFunctionApp(
+  `${projectName}-functions`,
+  {
+    resourceGroup,
+    archive: new pulumi.asset.FileArchive('./functions'),
+    version: '~3',
+    nodeVersion: '~10',
+    siteConfig: {
+      cors: { allowedOrigins: ['*'] }
+    }
+  }
+);
+
+export const nodeEndpoint = nodeApp.endpoint.apply((endpoint: string) =>
+  endpoint.replace(/api\/$/, '')
+);
