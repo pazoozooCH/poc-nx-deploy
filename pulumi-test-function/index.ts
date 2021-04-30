5ec8fed54e4d49114d2ac26cc79d87b9026b8561
Martin Meyer <martin.meyer@inftec.ch>
2021-04-30 11:59:13 +0200

    Added static index.html

--- pulumi-test-function/index.ts
+++ pulumi-test-function/index.ts
@@ -2,8 +2,6 @@ import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-native/resources";
 import * as storage from "@pulumi/azure-native/storage";
 
-console.log('### testing...');
-
 // Create an Azure Resource Group
 const resourceGroup = new resources.ResourceGroup("resourceGroup");
 
@@ -16,7 +14,26 @@ const storageAccount = new storage.StorageAccount("sa", {
     kind: storage.Kind.StorageV2,
 });
 
+// Enable static website support
+const staticWebsite = new storage.StorageAccountStaticWebsite("staticWebsite", {
+  accountName: storageAccount.name,
+  resourceGroupName: resourceGroup.name,
+  indexDocument: "index.html",
+});
+
+// Upload the file
+const indexHtml = new storage.Blob("index.html", {
+  resourceGroupName: resourceGroup.name,
+  accountName: storageAccount.name,
+  containerName: staticWebsite.containerName,
+  source: new pulumi.asset.FileAsset("index.html"),
+  contentType: "text/html",
+});
+
 // Export the primary key of the Storage Account
 const storageAccountKeys = pulumi.all([resourceGroup.name, storageAccount.name]).apply(([resourceGroupName, accountName]) =>
     storage.listStorageAccountKeys({ resourceGroupName, accountName }));
 export const primaryStorageKey = storageAccountKeys.keys[0].value;
+
+// Web endpoint to the website
+export const staticEndpoint = storageAccount.primaryEndpoints.web;
